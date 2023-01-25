4273dda85081149f4697c917263e95462683469d
seunghunbang <upi21x@gmail.com>
2023-01-25 10:29:51 +0200

    ts with nodejs & express to create CRUD process with using POSTMAN

---
+++ src/controllers/todos.ts
@@ -0,0 +1,50 @@
+import { Request, Response, NextFunction, RequestHandler } from "express";
+import { Todo } from "../models/todo";
+const TODOS: Todo[] = [];
+
+type PARAMID = { id: string };
+export const createTodo: RequestHandler = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const text = (req.body as { text: string }).text;
+
+  const newTodo = new Todo(Math.random().toString(), text);
+
+  TODOS.push(newTodo);
+
+  res.status(201).json({ message: "Created the todo!", createTodo: newTodo });
+};
+
+export const getTodos: RequestHandler = (req, res, next) => {
+  res.json({ todos: TODOS });
+};
+
+export const updateTodo: RequestHandler<PARAMID> = (req, res, next) => {
+  const todoId = req.params.id;
+
+  const updatedText = (req.body as { text: string }).text;
+
+  const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+
+  if (todoIndex < 0) {
+    throw new Error("Could not find index");
+  }
+
+  TODOS[todoIndex] = new Todo(TODOS[todoIndex].id, updatedText);
+
+  res.json({ message: "Updated", updatedTodo: TODOS[todoIndex] });
+};
+
+export const deleteTodo: RequestHandler<PARAMID> = (req, res, next) => {
+  const todoId = req.params.id;
+
+  const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+  if (todoIndex < 0) {
+    throw new Error("Could not find index");
+  }
+  TODOS.splice(todoIndex, 1);
+
+  res.json({ message: "Todo deleted" });
+};
